c0be3984a8bd684171ee8999677100a028df8f4b
Jill Rafferty <jill.rafferty@gmail.com>
2014-08-02 13:57:39 +0100

    Amended email headers - Added domain sender email and appended X-mailer

--- public_html/contact.php
+++ public_html/contact.php
@@ -3,6 +3,7 @@ $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_phone = $_POST['cf_phone'];
 $field_message = $_POST['cf_message'];
+$field_domainsenderemail = 'meathway@meathway.com';
 
 $mail_to = 'jillrafferty1@hotmail.com';
 $subject = 'Meathway - Enquiry from a site visitor '.$field_name;
@@ -12,8 +13,8 @@ $body_message .= 'E-mail: '.$field_email."\n";
 $body_message .= 'Phone: '.$field_phone."\n";
 $body_message .= 'Message: '.$field_message;
 
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+$headers = 'From: '.$field_domainsenderemail."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n".'X-Mailer: PHP/' . phpversion();
 
 
  $mail_status = mail($mail_to, $subject, $body_message, $headers);
